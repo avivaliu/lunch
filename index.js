4b4c099a739629998acf1a63032e30a20446423c
avivaliu <aviva@Users-MacBook-Pro-2.local>
2014-05-24 13:24:23 +0800

    add client vendor libs

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
-app.get('/', function(){
+app.get('/', function(req, res, next){
   res.render('index.html');
 });
-app.use(express.static())
+app.use(express.static(__dirname + '/public'));
+app.listen(8050);
