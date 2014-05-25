b095000b4dae5adc75f49cdf6c5ddc8fa6c39aa9
avivaliu <aviva@Users-MacBook-Pro-2.local>
2014-05-25 16:29:00 +0800

    Button add restaurant

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1360,11 +1360,14 @@ function lunchDraw(){
   var random = (Math.floor(Math.random() * restaurant.length));
   alert(restaurant[random].name);
 }
+
+function showForm(){
+  $('#form_add_rest').removeClass('hidden');
+}
   
 function init(){
-  $('#lunch_draw').click(function(){
-    lunchDraw();
-  });
+  $('#lunch_draw').click(lunchDraw);
+  $('#add_restaurant').click(showForm);
 }
 
 $(document).ready(init);
