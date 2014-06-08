496de68a0a7885f18edc9ba05eed888058f1738f
ethreys <ethreys@dev.env>
2014-06-08 10:35:38 +0800

    add delete canteen query parameter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 var express = require('express');
-var handleUpdate = require('./handle-update.js').handle;
+var handleUpdate = require('./handle-update.js').handleUpdate;
+var handleDelete = require('./handle-update.js').handleDelete;
 
 //use busboy to handle file uploading
 var app = express();
@@ -10,6 +11,7 @@ app.engine('html', require('ejs').renderFile);
 app.get('/', function(req, res, next){
   res.render('index.html');
 });
+app.get('/delete', handleDelete);
 app.post('/update', handleUpdate);
 app.use(express.static(__dirname + '/public'));
 app.listen(8050);
